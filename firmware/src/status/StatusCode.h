457a87c6cc040ac21d9251c2111b2444fde173cd
Bradley Gumm <daitsbradley@gmail.com>
2026-09-06 16:26:39 -0700

    formatting and add JOB_COMPLETE to signal its done.

--- firmware/src/status/StatusCode.h
+++ firmware/src/status/StatusCode.h
@@ -5,15 +5,20 @@
 #include <cstdint>
 
 enum class StatusCode : std::int8_t {
+    // this could be a timeout
     EXECUTION_ERROR = -10,
     UNEXPECTED_USB_DISCONNECT = -20,
     FIRMWARE_ERROR = -30,
+
     START_UP = 0,
+
     WAITING_FOR_JOB = 10,
     WAITING_FOR_CONFIG = 11,
     EXECUTING = 12,
     STEP_COMPLETED = 13,
     PAUSED = 14,
     RESUMED = 15,
+    JOB_COMPLETE = 16,
+
     FIRMWARE_UPDATE_IN_PROGRESS = 30
 };
