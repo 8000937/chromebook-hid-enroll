e7115479a7d24750730069242b315ff948d3ae55
Bradley Gumm <daitsbradley@gmail.com>
2026-09-06 16:30:43 -0700

    checkpoint

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,44 +1,66 @@
 #include <Arduino.h>
 #include <WiFi.h>
-#include <PubSubClient.h>
 #include "Adafruit_NeoPixel.h"
 #include "status/StatusLED.h"
 #include "status/StatusManager.h"
 #include "status/InformationCode.h"
 #include "settings.h"
+#include "execution/ExecutionManager.h"
 #include "messages/CommunicationsManager.h"
 #include "messages/StatusMessage.h"
+#include "usb/UsbManager.h"
 
+Stream &serial = Serial;
+String clientId = WiFi.macAddress();
+
+// I/O
 const Adafruit_NeoPixel led(1, 48, NEO_GRB + NEO_KHZ800);
-StatusLED statusLED(led, Serial, 50);
-StatusManager statusManager(statusLED, Serial);
+static USBHIDKeyboard Keyboard;
 
-CommunicationsManager communicationsManager(Serial);
+// subsystem controllers
+static StatusManager statusManager(led, serial);
+static CommunicationsManager communicationsManager(serial, clientId);
+static UsbManager usbManager(serial);
+static ExecutionManager executionManager(Keyboard,usbManager, serial);
 
-InformationCode informationStatus = InformationCode::NONE;
-StatusCode statusCode = StatusCode::START_UP;
+// status fields
+static volatile auto informationStatus = InformationCode::NONE;
+static volatile auto statusCode = StatusCode::START_UP;
+static StatusMessage currentStatusDetails;
 
-unsigned long timeStartedCurrentStep = 0;
-int8_t stepNumber = -1;
-int16_t jobId = -1;
-int16_t configVersion = -1;
+// job state
+static unsigned long timeStartedCurrentStep = 0;
+static int8_t stepNumber = -1;
+static int16_t jobId = -1;
+static int16_t configVersion = -1;
 
-const String clientId = WiFi.macAddress();
+
+// placeholder test steps.
+static ExecutionStep executionSteps[] = {
+    ExecutionStep(0, ExecutionStepType::WAIT_FOR_USB_CONNECT, "", nullptr, 0, false, 0, 1000, false),
+    ExecutionStep(1, ExecutionStepType::DELAY, "", nullptr, 0, false, 10000, 10000, false),
+    ExecutionStep(2, ExecutionStepType::TYPE_STRING, "Hello World", nullptr, 0, false, 0, 10000, false)
+};
+static uint8_t stepCount = 3;
+
+// controller state
+static volatile bool running = true;
 const String firmwareVersion = "0.0.1";
 
-StatusMessage currentStatusDetails;
+// timing
+static unsigned long timer = 0;
+constexpr unsigned long executionDelay = 200;
 
 
-
-unsigned long timer = 0;
 void onWiFiConnected(WiFiEvent_t event, WiFiEventInfo_t info) {
 
     Serial.println("Connected to WiFi");
 }
 void onWiFiGotIP(WiFiEvent_t event, WiFiEventInfo_t info) {
     informationStatus = InformationCode::NONE;
-    Serial.print("IP Address assigned: ");
-    Serial.println(WiFi.localIP());
+    serial.print("IP Address assigned: ");
+    serial.println(WiFi.localIP());
+    // communicationsManager.reconnect(currentStatusDetails);
 }
 void onWiFiDisconnected(WiFiEvent_t event, WiFiEventInfo_t info) {
     informationStatus = InformationCode::NO_WIFI;
@@ -48,7 +70,8 @@ void onWiFiDisconnected(WiFiEvent_t event, WiFiEventInfo_t info) {
 
     WiFi.begin(App::Settings::ssid.c_str(), App::Settings::password.c_str());
 }
-void updateCurrentStatusDetails() {
+
+static void updateCurrentStatusDetails() {
     currentStatusDetails.informationCode = static_cast<int8_t>(informationStatus);
     currentStatusDetails.statusCode = static_cast<int8_t>(statusCode);
     currentStatusDetails.configVersion = configVersion;
@@ -69,7 +92,10 @@ void onStatusChange(InformationCode newInfoCode, InformationCode oldInfoCode, St
         if (newStatusCode == StatusCode::STEP_COMPLETED) {
             //false is set for testing. this will come from the step eventually.
             //timeStartedCurrentStep will be set during the execution loop.
-            communicationsManager.stepCompleted(StepCompletedMessage(currentStatusDetails, millis()-timeStartedCurrentStep, false));
+
+            //TODO i think this stepCompleted thing could also represent a execution failed status. maybe make this more general
+            StepCompletedMessage msg(currentStatusDetails, millis()-timeStartedCurrentStep, false);
+            communicationsManager.stepCompleted(msg);
         }
         else {
             communicationsManager.sendStatusChange(currentStatusDetails);
@@ -93,16 +119,21 @@ void onStatusChange(InformationCode newInfoCode, InformationCode oldInfoCode, St
 
 }
 
-// remove after testing
-constexpr StatusCode codes[]= {StatusCode::EXECUTING, StatusCode::WAITING_FOR_CONFIG, StatusCode::EXECUTION_ERROR, StatusCode::UNEXPECTED_USB_DISCONNECT};
 
-StatusCode current = codes[0];
-//end remove after testing
-
+static void usbEventCallback(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data) {
+    serial.println(event_id);
+    usbManager.usbStatusCallback(arg, event_base, event_id, event_data);
+}
 
 
 void setup() {
     Serial.begin(115200);
+
+    delay(1000);
+
+    USB.onEvent(usbEventCallback);
+    usbManager.setup(Keyboard);
+
     // Initial setup of the status message
     // these shouldnt change
     currentStatusDetails.clientId = clientId;
@@ -110,24 +141,80 @@ void setup() {
     // the rest WILL change.
     updateCurrentStatusDetails();
 
+    WiFi.persistent(false);
+    WiFi.setAutoReconnect(true);
+
+
     statusManager.onStatusChange(onStatusChange);
+
     WiFi.onEvent(onWiFiConnected, WiFiEvent_t::ARDUINO_EVENT_WIFI_STA_CONNECTED);
     WiFi.onEvent(onWiFiGotIP, WiFiEvent_t::ARDUINO_EVENT_WIFI_STA_GOT_IP);
     WiFi.onEvent(onWiFiDisconnected, WiFiEvent_t::ARDUINO_EVENT_WIFI_STA_DISCONNECTED);
     WiFi.begin(App::Settings::ssid.c_str(), App::Settings::password.c_str());
+
     informationStatus = InformationCode::NO_WIFI;
 }
 
 
 void loop() {
-    if (timer == 0 || millis() - timer > 10000) {
-        const long r = random(0,4);
-        if (r <= 3) {
-            Serial.println(r);
-            current = codes[r];
+    // check status of subsystems.
+    if (WiFi.isConnected()) {
+        if (!communicationsManager.isMqttConnected()) {
+            communicationsManager.reconnectMqtt(currentStatusDetails);
+            running = false;
+        }
+        else {
+            running = true;
+        }
+    }
+    else {
+        running = false;
+    }
+
+    if ((millis() - timer) % 1000 <= 999) {
+        if (!usbManager.isConnected()) {
+            // TODO should this reset the step we're on?
+            statusCode = StatusCode::UNEXPECTED_USB_DISCONNECT;
+            running = false;
+        }
+        if (!running || statusCode == StatusCode::EXECUTION_ERROR) {
+            // TODO fix this -- doesnt print negative statuses well.. and these are all negative.
+            serial.printf("Program halted: %i\n", static_cast<int>(statusCode));
+        }
+    }
+
+    // main execution loop
+    if (statusCode != StatusCode::EXECUTION_ERROR && running && (timer == 0 || millis() - timer > executionDelay)) {
+        if (stepNumber == -1 && stepCount > 0) {
+            stepNumber = 0;
+            updateCurrentStatusDetails();
+        }
+
+        // if no errors, steps are defined, and we are not at the end of the step array
+        if (stepCount > 0 && stepNumber > -1 && stepCount != stepNumber) {
+            const ExecutionStatus execStatus = executionManager.execute(executionSteps[stepNumber]);
+            if ( ExecutionStatus::COMPLETE == execStatus) {
+                statusCode = StatusCode::STEP_COMPLETED;
+                stepNumber++;
+                updateCurrentStatusDetails();
+            }
+            else if (ExecutionStatus::TIMED_OUT == execStatus) {
+                statusCode = StatusCode::EXECUTION_ERROR;
+            }
+        }
+        // if no errors, steps are defined, and we're at the end.
+        else if (stepCount > 0 && stepNumber == stepCount) {
+                statusCode = StatusCode::JOB_COMPLETE;
         }
         timer = millis();
     }
+
+
     // Test to show realtime status changes and confirm it works as expected.
-    statusManager.setStatus(current, informationStatus);
+    statusManager.setStatus(statusCode, informationStatus);
+
+    // just a workaround to let us send the status update, and not lose the actual status.
+    if (StatusCode::STEP_COMPLETED == statusCode) {
+        statusCode = StatusCode::EXECUTING;
+    }
 }
